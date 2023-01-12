5adf502933ad2b8be322658de0431299174cad5a
Victor Arvidsson <victarv@gmail.com>
2023-01-12 20:49:21 +0100

    Project completed

--- Project/NeuralNetwork.py
+++ Project/NeuralNetwork.py
@@ -2,118 +2,115 @@ import numpy as np
 from typing import Any, Callable
 from enum import auto, Enum
 from dataclasses import dataclass
-import math
-import random
-import matplotlib.pyplot as plt
+
 
 class Activation(Enum):
     SIGMOID = auto(),
-    RELU = auto(),
-    # SOFTMAX = auto(), // for later
     TANH = auto()
 
-@dataclass
-class ActivationFunction():
-    func: Callable[[float], float]
-    dfunc: Callable[[float], float]
 
 class NeuralNetwork():
-    @staticmethod
-    def sigmoid(x: float) -> float:
-        return 1/(1+math.exp(-x))
-    
-    @staticmethod
-    def dsigmoid(x: float) -> float:
-        return x * (1 - x)
-    
-    @staticmethod
-    def relu(x: float) -> float:
-        return max(0, x)
-    
-    @staticmethod
-    def drelu(x: float) -> float:
-        return 1.0 if x > 0.0 else 0.0
-    
-    # @staticmethod // for later
-    # def softmax(x: float):
-    #     return 
+    @dataclass
+    class ActivationFunction():
+        func: Callable[[float], float]
+        dfunc: Callable[[float], float]
 
     @staticmethod
-    def tanh(x: float) -> float: 
+    def sigmoid(x):
+        np.clip(x, -500, 500)
+        return 1/(1+np.exp(-x))
+
+    @staticmethod
+    def dsigmoid(x):
+        return x * (1 - x)
+
+    @staticmethod
+    def tanh(x) -> float:
         return np.tanh(x)
 
     @staticmethod
-    def dtanh(x: float):
+    def dtanh(x):
         return 1 - x**2
 
     activationFunctions = {
         Activation.SIGMOID: ActivationFunction(sigmoid.__get__(object), dsigmoid.__get__(object)),
-        Activation.RELU: ActivationFunction(relu.__get__(object), drelu.__get__(object)),
-        Activation.TANH: ActivationFunction(tanh.__get__(object), dtanh.__get__(object))
+        Activation.TANH: ActivationFunction(
+            tanh.__get__(object), dtanh.__get__(object))
     }
 
     def __init__(self, input_l: int, hidden_l: "list[int]", output_l: int, activation: Activation, lr: float = 0.01):
         self._activationFunction = NeuralNetwork.activationFunctions[activation]
         self._lr = lr
 
-        self._input_l = np.zeros((input_l, 1))# We initilize the input as a as an input layer of 0s
-        self._hidden_l: "list[np.ndarray[np.float64, Any]]" = [] # The hidden layer is a list of arrays that contain the amount of nodes
-        for h_l in hidden_l: #Here we create a matrix for each hidden layer in the list, array -> matrix 
+        # Create the layers of the network and initialize them to 0
+        self._input_l = np.zeros((input_l, 1))
+        self._hidden_l: "list[np.ndarray[np.float64, Any]]" = []
+        for h_l in hidden_l:
             self._hidden_l.append(np.zeros((h_l, 1)))
-        self._output_l = np.zeros((output_l, 1)) #We do the same as in the input layer
+        self._output_l = np.zeros((output_l, 1))
+
+        # Create the weights of the network and initialize them randomly following a uniform distribution
         self._weightsHH: "list[np.ndarray[Any, np.dtype[np.float64]]]" = []
         self._weightsBH: "list[np.ndarray[Any, np.dtype[np.float64]]]" = []
-        self._weightsIH = np.random.uniform(-1, 1, (hidden_l[0], input_l)) #We create a randomized matrix from the normal dist to create a matrix with hidden layer rows and input layer columns, aka 3x3
+        self._weightsIH = np.random.uniform(-1, 1, (hidden_l[0], input_l))
         for h_l in range(len(hidden_l)-1):
             shape = (hidden_l[h_l+1], hidden_l[h_l])
             self._weightsHH.append(np.random.uniform(-1, 1, shape))
             self._weightsBH.append((np.random.uniform(-1, 1, (shape[0], 1))))
         self._weightsHO = np.random.uniform(-1, 1, (output_l, hidden_l[-1]))
-        
         self._weightBI = np.random.uniform(-1, 1, (hidden_l[0], 1))
         self._weightBO = np.random.uniform(-1, 1, self._output_l.shape)
 
-
     def predict(self, x: "list[float]"):
-        activation = np.vectorize(self._activationFunction.func)
-        inputs = np.array(x).reshape((len(x),1))
-        assert inputs.shape[0] == self._input_l.shape[0]
-        tempIH = np.matmul(self._weightsIH, inputs) #we muliply the weights of the input -> hidden with inputs to give us a matrix of 3x2
-        tempIH = np.add(tempIH, self._weightBI)
+        activation = self._activationFunction.func
+        self._input_l = np.array(x).reshape((len(x), 1))
 
+        # Input to hidden
+        tempIH = np.matmul(self._weightsIH, self._input_l)
+        tempIH = np.add(tempIH, self._weightBI)
         self._hidden_l[0] = activation(tempIH)
+
+        # Hidden to hidden
         for h_l in range(len(self._hidden_l)-1):
-            self._hidden_l[h_l+1] = np.matmul(self._weightsHH[h_l], self._hidden_l[h_l]) #We multiply the weights of the hidden layer by the hidden layer 
-            self._hidden_l[h_l+1] = np.add(self._hidden_l[h_l+1], self._weightsBH[h_l]) #We add the weights of the hidden layer by the hidden layer 
+            self._hidden_l[h_l +
+                           1] = np.matmul(self._weightsHH[h_l], self._hidden_l[h_l])
+            self._hidden_l[h_l +
+                           1] = np.add(self._hidden_l[h_l+1], self._weightsBH[h_l])
             self._hidden_l[h_l+1] = activation(self._hidden_l[h_l+1])
-        
+
+        # Hidden to output
         self._output_l = np.matmul(self._weightsHO, self._hidden_l[-1])
         self._output_l = np.add(self._output_l, self._weightBO)
         self._output_l = activation(self._output_l)
         return self._output_l.reshape(1, -1).tolist()[0]
 
     def train(self, x: "list[float]", answers_lst: "list[float]"):
-        derivation = np.vectorize(self._activationFunction.dfunc)
+        derivation = self._activationFunction.dfunc
         predictions_lst = self.predict(x)
-        predictions = np.array(predictions_lst).reshape((len(predictions_lst),1))
-        answers = np.array(answers_lst).reshape((len(answers_lst),1))
-        #Backpropagate errors
+        predictions = np.array(predictions_lst).reshape(
+            (len(predictions_lst), 1))
+        answers = np.array(answers_lst).reshape((len(answers_lst), 1))
+
+        # Backpropagate errors
+        # Output to hidden
         errorO = answers - predictions
         errorHLast = np.matmul(self._weightsHO.T, errorO)
 
+        # Hidden to Hidden
         errorsH = [errorHLast]
         for n in range(len(self._hidden_l) - 2, -1, -1):
             errorHH = np.matmul(self._weightsHH[n].T, errorsH[0])
             errorsH.insert(0, errorHH)
-        
-        # adjust weights
+
+        # Apply gradient descent
+        # Output to hidden
         gradient = derivation(self._output_l)
         gradient *= errorO * self._lr
 
         self._weightBO += gradient
         deltaHidden = np.matmul(gradient, self._hidden_l[-1].T)
         self._weightsHO += deltaHidden
-        
+
         # Hidden to hidden
         for n in range(len(self._hidden_l)-1, 0, -1):
             gradient = derivation(self._hidden_l[n])
@@ -124,31 +121,47 @@ class NeuralNetwork():
 
         # Hidden to input
         gradient = derivation(self._hidden_l[0])
-        
+
         gradient *= errorsH[0] * self._lr
         self._weightBI += gradient
         self._weightsIH += np.matmul(gradient, self._input_l.T)
 
 
 if __name__ == '__main__':
-    x = [[0., 0.], [0., 1.], [1., 0.], [1., 1.]]
-    y = [[1], [1], [1], [-1]]
-    nn = NeuralNetwork(2, [6], 1, Activation.TANH, 0.01)
-    # print(nn.predict([0., 0.]), nn.predict([0., 1.]), nn.predict([1., 0.]), nn.predict([1., 1.]))
-    while True:
-        for _ in range(5000):
-            i = random.randint(0, 3)
+    import matplotlib.pyplot as plt
+    x = [[-1., -1.], [-1., 1.], [1., -1.], [1., 1.]]
+    y = [[-1.], [1.], [1.], [-1.]]
+    nn = NeuralNetwork(2, [3], 1, Activation.TANH, 0.01)
+    for _ in range(100):
+        for j in range(100):
+            i = j % 4
             nn.train(x[i], y[i])
         pred = []
-        for i in range(20):
+        for i in range(-50, 50):
             t = []
-            for j in range(20):
-                t.append(nn.predict([i/20, j/20])[0])
+            for j in range(-50, 50):
+                t.append(nn.predict([i/50, j/50])[0])
             pred.append(t)
-        plt.imshow(np.array(pred), cmap="winter")
+        pred = (np.array(pred) + 1)/2
+        plt.imshow(pred, cmap="winter")
         plt.colorbar()
         plt.gca().invert_yaxis()
+        plt.xticks([0, 100], [0, 1])
+        plt.yticks([0, 100], [0, 1])
         plt.draw()
-        plt.pause(0.0001)
+        plt.pause(0.001)
         plt.clf()
-        print(f"{nn.predict([0., 0.])[0]:.3f}", f"{nn.predict([0., 1.])[0]:.3f}", f"{nn.predict([1., 0.])[0]:.3f}", f"{nn.predict([1., 1.])[0]:.3f}")
+        print(f"{nn.predict([-1., -1.])[0]:.3f}", f"{nn.predict([-1., 1.])[0]:.3f}",
+              f"{nn.predict([1., -1.])[0]:.3f}", f"{nn.predict([1., 1.])[0]:.3f}")
+    pred = []
+    for i in range(-50, 50):
+        t = []
+        for j in range(-50, 50):
+            t.append(nn.predict([i/50, j/50])[0])
+        pred.append(t)
+    plt.imshow(np.array(pred), cmap="winter")
+    plt.colorbar()
+    plt.gca().invert_yaxis()
+    plt.xticks([0, 100], [0, 1])
+    plt.yticks([0, 100], [0, 1])
+    plt.show()
---
+++ Project/README.md
@@ -0,0 +1,33 @@
+# Required libraries
+## NeuralNetwork.py
+To use the nerual network class, you need the following libraries:
+* Numpy
+
+To run the XOR example in the file, you need the following libraries:
+* Numpy
+* Matplotlib
+
+## MNIST.ipynb
+To run the notenook with the MNIST example, you need the following libraries:
+* Jupyter Notebook
+* Numpy
+* Pandas
+* Matplotlib
+* Scikit-learn
+
+# Using the neural network class
+To use the nerual network class, you need to instansiate a NeuralNetwork object. The constructor takes four parameters required parameters and one optional parameter. These are the following:
+* input_l: int - The number of nodes in the input layer
+* hidden_l: list[int] - A list of integers specifying the number of nodes in each hidden layer
+* output_l: int - The number of nodes in the output layer
+* activation: Activation: The activation function to use. Use the Activation enum in the same module.
+* lr: Optional[float] - The learning rate for the model
+
+The class provides two functions, one for making predictions and one for training the model.
+
+The prediction function has one parameter, a list of the values for the data point that you want to predict. The function returns a list of values representing each of the nodes in the output layer.
+
+The training function takes two parameters, a list of the values for the data point that you want to train, and a list of the expected outputs of the network. The function does not return anything.
+
+# Using the notebook
+Open the notebook and run it. With the current parameters, the training will take about 60 minutes to complete.
